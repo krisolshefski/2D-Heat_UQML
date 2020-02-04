6985503df6e9e5276cccbf14151c19da036cf66b
Kris <krisolshefski@vt.edu>
2020-02-04 18:24:14 -0500

    Initial Overleaf Import

---
+++ code/input.py
@@ -0,0 +1,18 @@
+# Input values required for 2D Heat Equation Test case
+
+# Domain size (Lx = Length in x-direction)
+Lx = 100
+Ly = 1
+
+# Mesh Size (Nx = Number of Cells in the x-direction)
+Nx = 100
+Ny = 100
+
+# Time step
+dt = 0.01
+
+# End Time
+t_end = 1000
+
+# Diffusion Coefficient
+eps = 0.05
